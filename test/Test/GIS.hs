e80dc5754a90cebe8aa674b3879a8fc964605ed7
Eric Bailey <eric@ericb.me>
2024-08-02 18:35:14 -0500

    fix(haskell): get the tests passing

--- test/Test/GIS.hs
+++ test/Test/GIS.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Test.GIS where
 
-import Data.GIS
+import Data.GIS (int)
+import Data.Modular (ℤ, type (/))
+import Data.Monoid (Sum (..))
+import Data.Pitch (Pitch)
 import qualified Data.PitchClass.Chromatic as Chromatic
 import qualified Data.PitchClass.Diatonic as Diatonic
-import Test.Tasty
-import Test.Tasty.HUnit
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 test_lewin_2_1_1 :: TestTree
 test_lewin_2_1_1 =
-  let int' = int gisDiatonicPitch
+  let int' = int :: Pitch Diatonic.PitchClass -> Pitch Diatonic.PitchClass -> Sum Int
    in testGroup
         "Diatonic pitch examples (Lewin, 2.1.1)"
         [ testCase "int(C4, C4) = 0" $
@@ -33,7 +39,7 @@ test_lewin_2_1_1 =
 
 test_lewin_2_1_2 :: TestTree
 test_lewin_2_1_2 =
-  let int' = int pSpace
+  let int' = int :: Pitch Chromatic.PitchClass -> Pitch Chromatic.PitchClass -> Sum Int
    in testGroup
         "p-space examples (Lewin, 2.1.2)"
         [ testCase "int(C4, D4) = 2" $
@@ -50,7 +56,7 @@ test_lewin_2_1_2 =
 
 test_lewin_2_1_3 :: TestTree
 test_lewin_2_1_3 =
-  let int' = int pcSpace
+  let int' = int :: Chromatic.PitchClass -> Chromatic.PitchClass -> (ℤ / 12)
    in testGroup
         "pc-space examples (Lewin, 2.1.3)"
         [ testCase "int(8, 1) = 5" $
@@ -65,7 +71,7 @@ test_lewin_2_1_3 =
 
 test_lewin_2_1_4 :: TestTree
 test_lewin_2_1_4 =
-  let int' = int gisDiatonicPitchClass
+  let int' = int :: Diatonic.PitchClass -> Diatonic.PitchClass -> (ℤ / 7)
    in testGroup
         "Diatonic pitch class examples (Lewin, 2.1.4)"
         [ testCase "int(D, D) = 0" $
