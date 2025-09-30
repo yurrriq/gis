59880e295e913d3f6ad1b8c939a46bae7f893148
Eric Bailey <eric@ericb.me>
2025-09-29 23:10:37 -0500

    build(haskell): tidy dependencies

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -1,23 +1,6 @@
 { ... }:
 
 {
-  flake.overlays.haskell = _final: prev: {
-    haskellPackages = prev.haskellPackages.override {
-      overrides = _hfinal: hprev: {
-        acts = hprev.callCabal2nix "acts"
-          (prev.fetchFromGitHub {
-            owner = "sheaf";
-            repo = "acts";
-            rev = "3588191d44771fcd9f86e664283c453081750a07"; # v0.3.1.2
-            hash = "sha256-2QaMRl8Fg6QnrAVkQesIMXeKg2B6XMtAMSAWr/0X8LQ=";
-          })
-          { };
-        typelits-witnesses =
-          prev.haskell.lib.markUnbroken hprev.typelits-witnesses;
-      };
-    };
-  };
-
   perSystem = { pkgs, self', ... }: {
     devShells.haskell = pkgs.mkShell {
       inputsFrom = [
